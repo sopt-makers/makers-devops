e185b6fd25257bc16b3cb87a81c5fa24dfa600e5
wuzoo <wndyd9911@gmail.com>
2026-03-19 19:48:50 +0900

    chore: util.ts > utils/

--- servers/mumu/src/index.ts
+++ servers/mumu/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { createWebhookRouter } from "./webhook";
 import { redisStorage } from "./redis";
-import { assertNonNullish } from "./util";
+import { assertNonNullish } from "./utils/assert";
 
 process.on("unhandledRejection", (reason) => {
   console.error("Unhandled rejection:", reason);
