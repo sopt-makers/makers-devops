86eaaf5285cca0672beb0a6622742645c8fa92c6
wuzoo <wndyd9911@gmail.com>
2026-03-28 17:49:52 +0900

    feat: redis를 통한 create/find 인터페이스 제공

--- packages/slack/src/index.ts
+++ packages/slack/src/index.ts
@@ -1,2 +1,44 @@
+import type { SlackThreadData, SlackThreadMessage } from "./types";
+import { slackClient } from "./client";
+import { getSlackThreadData, setSlackThreadData } from "./redis";
+
 export * from "./client";
 export * from "./types";
+export * from "./redis";
+
+export const createSlackThread = async (id: string, message: SlackThreadMessage) => {
+  const { channel, text } = message;
+
+  try {
+    const response = await slackClient.chat.postMessage({
+      channel,
+      text,
+    });
+
+    if (response.ts) {
+      const data: SlackThreadData = {
+        id,
+        version: 1,
+        channel,
+        thread_ts: response.ts,
+      };
+      setSlackThreadData(id, data);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const findSlackThread = async (id: string) => {
+  try {
+    const data = await getSlackThreadData(id);
+
+    if (data) {
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  return null;
+};
--- packages/slack/src/types.ts
+++ packages/slack/src/types.ts
@@ -1,20 +1,18 @@
-import type { ChatPostMessageArguments } from "@slack/web-api";
 import { z } from "zod";
 
-/** slack 메세지 스키마 */
-export const slackThreadMessageSchema = z.object({
-  channel: z.string(),
-  thread_ts: z.string(),
-});
-
-/** storage에 저장되는 스레드 데이터 스키마 */
+/** Storage에 저장되는 스레드 데이터 스키마 */
 export const slackThreadDataSchema = z.object({
   id: z.string(),
   version: z.literal(1),
   channel: z.string(),
-  ts: z.string(),
+  thread_ts: z.string(),
 });
 
-export type SlackMessage = Partial<ChatPostMessageArguments> & { text: string };
-export type SlackThreadMessage = z.infer<typeof slackThreadMessageSchema>;
+/** 슬랙 메시지 스키마 */
+export const slackThreadMessageSchema = z.object({
+  channel: z.string(),
+  thread_ts: z.string().optional(),
+});
+
+export type SlackThreadMessage = z.infer<typeof slackThreadMessageSchema> & { text: string };
 export type SlackThreadData = z.infer<typeof slackThreadDataSchema>;
