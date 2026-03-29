784f140a6dd4ac843d1b1dfcb4ddfe5055e1a232
wuzoo <wndyd9911@gmail.com>
2026-03-29 22:29:13 +0900

    refactor: webhook router

--- servers/mumu/src/webhook.ts
+++ servers/mumu/src/webhook.ts
@@ -1,45 +1,10 @@
-import { Router, type Request, type Response } from "express";
-import { pullRequestSchema, pullRequestReviewCommentSchema } from "@makers-devops/github";
-import { handlePullRequest } from "./handler/pull_request";
-import { handlePullRequestReviewComment } from "./handler/pull_request-review-comment";
-import { isValidRepository } from "./config";
+import { Router } from "express";
+import { handleGithubWebhook } from "./handler/github-webhook";
 
 export function createWebhookRouter(): Router {
   const router = Router();
 
-  router.post("/webhook", async (req: Request, res: Response) => {
-    const event = req.headers["x-github-event"];
-
-    const fullName: string | undefined = req.body?.repository?.full_name;
-    const repoName = fullName?.split("/")[1];
-
-    if (!isValidRepository(repoName)) {
-      return res.status(400).json({ error: `Invalid repository: ${fullName}` });
-    }
-
-    /**
-     * Webhook API는 10초 동안 응답하지 않으면 delivery하지 않음
-     * 각 핸들러들을 백그라운드에서 실행하고 바로 응답
-     */
-    res.status(200).send("Webhook received");
-
-    try {
-      switch (event) {
-        case "pull_request": {
-          handlePullRequest(pullRequestSchema.parse(req.body)).then((res) => console.log(res));
-          break;
-        }
-        case "pull_request_review_comment": {
-          handlePullRequestReviewComment(pullRequestReviewCommentSchema.parse(req.body)).then((res) =>
-            console.log(res),
-          );
-          break;
-        }
-      }
-    } catch (err) {
-      console.error(`[${event}] webhook 처리 실패:`, err);
-    }
-  });
+  router.post("/webhook", handleGithubWebhook);
 
   return router;
 }
