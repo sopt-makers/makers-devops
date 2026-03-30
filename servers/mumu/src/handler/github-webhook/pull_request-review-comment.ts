81a6eb92c454825578cd4078ff0a8ceb4f6522c4
wuzoo <wndyd9911@gmail.com>
2026-03-30 22:10:24 +0900

    refactor: handle req/res

--- servers/mumu/src/handler/github-webhook/pull_request-review-comment.ts
+++ servers/mumu/src/handler/github-webhook/pull_request-review-comment.ts
@@ -1,16 +1,19 @@
-import type { PullRequestReviewComment } from "@makers-devops/github";
+import type { Request, Response } from "express";
+import { pullRequestReviewCommentSchema } from "@makers-devops/github";
 import { createPullRequestReviewCommentReply } from "../../slack";
 
-export const handlePullRequestReviewComment = async (payload: PullRequestReviewComment) => {
+export const handlePullRequestReviewComment = async (req: Request, res: Response) => {
+  const payload = pullRequestReviewCommentSchema.parse(req.body);
+
   if (payload.action !== "created") {
-    return JSON.stringify({ success: false, message: "Review comment action skipped." });
+    return res.json({ success: false, message: "Review comment action skipped." });
   }
 
   const result = await createPullRequestReviewCommentReply(payload);
 
   if (!result) {
-    return JSON.stringify({ success: false, message: "Slack thread reply failed" });
+    return res.json({ success: false, message: "Slack thread reply failed" });
   }
 
-  return JSON.stringify({ success: true, message: "Review comment processed successfully", result });
+  return res.json({ success: true, message: "Review comment processed successfully", result });
 };
